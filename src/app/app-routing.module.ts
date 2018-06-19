60f6a2f190212c89c7c3256e2935a09447d38907
N <007325@spi-global.com>
2018-06-19 15:47:42 +0530

    user collections page updated

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,13 +4,15 @@ import { HomeComponent } from './home/home.component';
 import { CarouselComponent } from './carousel/carousel.component';
 import { ProdDetailsComponent } from './prod-details/prod-details.component';
 import {ProductdetailsComponent} from './productdetails/productdetails.component';
+import { UsercollectionsComponent } from './usercollections/usercollections.component';
 
 const routes: Routes = [
   { path: '', redirectTo: '/home', pathMatch: 'full' },
   { path: 'home', component: HomeComponent },
   { path: 'carousel', component: CarouselComponent },
- { path: 'productdetail', component: ProdDetailsComponent },
-  { path: 'productdetails', component: ProductdetailsComponent }
+  { path: 'productdetail', component: ProdDetailsComponent },
+  { path: 'productdetails', component: ProductdetailsComponent },
+  { path: 'myColection', component: UsercollectionsComponent }
 ]
 @NgModule({
   exports: [ RouterModule ],
