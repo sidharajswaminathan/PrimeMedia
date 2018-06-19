e9b70862b8751e73df9f15266e684c7b46859c5e
K <007290@spi-global.com>
2018-06-19 11:55:49 +0530

    routing productdetails page added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { CarouselComponent } from './carousel/carousel.component';
+import {ProductdetailsComponent} from './productdetails/productdetails.component';
 
 const routes: Routes = [
   { path: '', redirectTo: '/home', pathMatch: 'full' },
   { path: 'home', component: HomeComponent },
   { path: 'carousel', component: CarouselComponent },
+  { path: 'productdetails', component: ProductdetailsComponent }
 
 ]
 @NgModule({
