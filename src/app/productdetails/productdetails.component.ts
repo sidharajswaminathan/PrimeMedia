6db61239c51c8b29183fe5b08c11c5f43baae4bd
sidharajgit87 <36883723+sidharajgit87@users.noreply.github.com>
2018-06-20 20:49:22 +0530

    modal component changes

--- src/app/productdetails/productdetails.component.ts
+++ src/app/productdetails/productdetails.component.ts
@@ -1,15 +1,21 @@
-import { Component, OnInit } from '@angular/core';
-
+import {Component, OnInit, ViewEncapsulation} from '@angular/core';
+import {NgbModal, ModalDismissReasons} from '@ng-bootstrap/ng-bootstrap';
+import { PrimeModalComponent } from '../prime-modal/prime-modal.component';
 @Component({
   selector: 'app-productdetails',
   templateUrl: './productdetails.component.html',
-  styleUrls: ['./productdetails.component.scss']
+  styleUrls: ['./productdetails.component.scss'],
+  encapsulation: ViewEncapsulation.None,
 })
 export class ProductdetailsComponent implements OnInit {
-
-  constructor() { }
-
+  showModal: Boolean = false;
+  constructor(private modalService: NgbModal) { }
+  closeResult: string;
   ngOnInit() {
   }
-
+  openVerticallyCentered() {
+    this.showModal = true;
+    const modalRef = this.modalService.open(PrimeModalComponent);
+    modalRef.componentInstance.name = 'World';
+  }
 }
