afa76afa45d02a647ac6a6117130812b6e14f690
sanjaykumar <sanjaykumar.k@spi-global.com>
2018-06-20 13:34:50 +0530

    fixes on new carousel

--- src/app/mycollections/mycollections.component.ts
+++ src/app/mycollections/mycollections.component.ts
@@ -25,7 +25,28 @@ export class MycollectionsComponent implements OnInit {
       'data': [
         {'src': '../assets/images/img1.png', 'id': '1'  },
         {'src': '../assets/images/img3.png', 'id': '2'  },
-        {'src': '../assets/images/img4.png', 'id': '3' }
+        {'src': '../assets/images/img4.png', 'id': '3' },
+        {'src': '../assets/images/img4.png', 'id': '4' },
+        {'src': '../assets/images/img2.png', 'id': '5' },
+        {'src': '../assets/images/img3.png', 'id': '6' },
+        {'src': '../assets/images/img4.png', 'id': '7' },
+        {'src': '../assets/images/img2.png', 'id': '8' },
+        {'src': '../assets/images/img3.png', 'id': '9'},
+        {'src': '../assets/images/img2.png', 'id': '10' },
+        {'src': '../assets/images/img2.png', 'id': '11'},
+        {'src': '../assets/images/img1.png', 'id': '12'},
+        {'src': '../assets/images/img4.png', 'id': '13'},
+        {'src': '../assets/images/img2.png', 'id': '14'},
+        {'src': '../assets/images/img1.png', 'id': '15'},
+        {'src': '../assets/images/img3.png', 'id': '16'},
+        {'src': '../assets/images/img1.png', 'id': '17'},
+        {'src': '../assets/images/img3.png', 'id': '18'},
+        {'src': '../assets/images/img1.png', 'id': '19'},
+        {'src': '../assets/images/img3.png', 'id': '20'},
+        {'src': '../assets/images/img1.png', 'id': '21'},
+        {'src': '../assets/images/img3.png', 'id': '22'},
+        {'src': '../assets/images/img1.png', 'id': '23'},
+        {'src': '../assets/images/img2.png', 'id': '24'}
       ]
     }
     console.log(this.carousel);
--- src/app/productlist/productlist.component.ts
+++ src/app/productlist/productlist.component.ts
@@ -1,17 +1,23 @@
 import { Component, OnInit } from '@angular/core';
 import { carouselData } from '../mock-appdata';
+import {carouselDragData} from '../usertype';
 @Component({
   selector: 'app-productlist',
   templateUrl: './productlist.component.html',
   styleUrls: ['./productlist.component.css']
 })
 export class ProductlistComponent implements OnInit {
-
+  carousel: object = carouselDragData;
   constructor() { }
   data: Array<any> = [];
   ngOnInit() {
 console.log(carouselData);
-    this.data = carouselData;
+    /*this.data = carouselData;*/
+    this.carousel = {
+      'configuration': {'deleteoption': false, 'editsave': false},
+      'data': carouselData
+    }
+
   }
 
 }
