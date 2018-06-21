496eaaa9d38ace034fa1bdaedaf93d6a717506bc
parthiban <Parthiban.V@spi-global.com>
2018-06-21 19:03:12 +0530

    search page fixed

--- src/app/mycollections/mycollections.component.ts
+++ src/app/mycollections/mycollections.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit, Input } from '@angular/core';
 import {NgbTabsetConfig} from '@ng-bootstrap/ng-bootstrap';
 import {carouselData} from '../mock-appdata';
 import {carouselDragData} from '../usertype';
+import {SharedserviceService} from '../sharedservice.service';
 
 @Component({
   selector: 'app-mycollections',
@@ -13,9 +14,10 @@ export class MycollectionsComponent implements OnInit {
   carousel: object = carouselDragData;
 
 
-  constructor(config: NgbTabsetConfig) {
+  constructor(config: NgbTabsetConfig, private sharedObj: SharedserviceService ) {
     config.justify = 'start';
     config.type = 'tabs';
+    this.sharedObj.globalObj.showBreadcrumb = false;
   }
 
   ngOnInit() {
