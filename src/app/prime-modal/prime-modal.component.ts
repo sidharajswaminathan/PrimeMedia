7d20aa446415392a78e3902bfed57a25e129622d
Rajadurai <rajadurai.p@spi-global.com>
2018-06-21 12:57:08 +0530

    Add new collection model updated

--- src/app/prime-modal/prime-modal.component.ts
+++ src/app/prime-modal/prime-modal.component.ts
@@ -8,14 +8,18 @@ import {NgbModal, NgbActiveModal} from '@ng-bootstrap/ng-bootstrap';
 export class PrimeModalComponent {
   @Input() name;
   showAdd: boolean = false;
+  collectName: string;
   closeResult: string;
+  colDAta: string;
   checkData: Array<any>;
+  currentUser: Array<any>;
   constructor(public activeModal: NgbActiveModal) {
   }
   
   ngOnInit(){
 
-    this.checkData = [{'name':'check1', 'id':'check1'},
+    this.checkData = [
+    {'name':'check1', 'id':'check1'},
     {'name':'check2', 'id':'check2'},
     {'name':'check3', 'id':'check3'},
     {'name':'check4', 'id':'check4'},
@@ -24,5 +28,10 @@ export class PrimeModalComponent {
     
  }
  
+ saveCollection() {
+     this.checkData.push({'name': this.colName,'id': this.colName });
+     this.showAdd= false;
+     localStorage.setItem('this.currentUser', this.checkData);
+ }
 
 }
