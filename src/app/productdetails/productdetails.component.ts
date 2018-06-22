72d17aebe1a8bf90e1f21cff70a74eb60f5b2fca
sidharajgit87 <36883723+sidharajgit87@users.noreply.github.com>
2018-06-22 21:18:58 +0530

    player navigation is implemented

--- src/app/productdetails/productdetails.component.ts
+++ src/app/productdetails/productdetails.component.ts
@@ -13,7 +13,7 @@ export class ProductdetailsComponent implements OnInit {
   showModal: Boolean = false;
   closeResult: string;
   prodDetailLabel: string;
-
+  prodDetails: any;
   constructor(private sharedObj: SharedserviceService, private modalService: NgbModal, private localstorage: LocalstorageService ) {
     console.log(this.localstorage.getLocaldata('catId'))
     this.sharedObj.globalObj.breadcrumbList = [
@@ -29,18 +29,41 @@ export class ProductdetailsComponent implements OnInit {
     modalRef.componentInstance.name = 'World';
   }
   ngOnInit() {
-    const prodDetail: any = this.localstorage.getLocaldata('prodDetail') && JSON.parse(this.localstorage.getLocaldata('prodDetail'))
-    console.log(prodDetail);
-    if (prodDetail && prodDetail.is_active) {
-      if (prodDetail.is_premium) {
+    this.prodDetails = this.localstorage.getLocaldata('prodDetail') && JSON.parse(this.localstorage.getLocaldata('prodDetail'))
+    console.log(this.prodDetails);
+    if (this.prodDetails && this.prodDetails.is_active) {
+      if (this.prodDetails.is_premium) {
         this.prodDetailLabel = 'Buy Now';
       } else {
-        if (!prodDetail.is_premium && !prodDetail.is_featured) {
+        if (!this.prodDetails.is_premium && !this.prodDetails.is_featured) {
           this.prodDetailLabel = 'Explore Now';
-        } else if (!prodDetail.is_premium && prodDetail.is_featured) {
+        } else if (!this.prodDetails.is_premium && this.prodDetails.is_featured) {
           this.prodDetailLabel = 'Register Now';
         }
       }
+    }
+  }
+
+  launchPlayer() {
+    let mediaurl: any = '';
+    const currentCategory: any = this.localstorage.getLocaldata('currentCategory');
+    if (currentCategory === 'Sommer time stories') {
+      mediaurl = 'http://sommerlearning.com/books/androcles-and-the-lion/';
+      window.open(mediaurl);
+    } else if (currentCategory === 'Phonics Adventure') {
+      if (this.prodDetails.category_name === 'eBooks' && this.prodDetailLabel === 'Explore Now') {
+        mediaurl = 'https://www.advancepublishing-dev.com/ereadermedialibrary/#/eReaderPlayer';
+        this.localstorage.setLocaldata('coursename', this.prodDetails.coursename);
+        this.localstorage.setLocaldata('category_name', this.prodDetails.category_name);
+        this.localstorage.setLocaldata('book_url', this.prodDetails.book_url);
+        /* this.localstorage.setLocaldata('mediaUserid',this.prodDetails.mediaUserid);*/
+        window.open(mediaurl + '/ereadermedialibrary/#/eReaderPlayer');
+      } else {
+        alert ('This is different type of product');
+      }
+    } else if (currentCategory === 'Number success') {
+        alert('Page needs to be developed');
+        /*window.open('https://www.advancepublishing-dev.com/sommer_learning/publicsite/numbersuccess');*/
     }
   }
 }
